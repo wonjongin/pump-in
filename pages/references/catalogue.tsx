29aedbfa3af4078dc0de589436772329bb809dfe
wonjongin <wji0227@naver.com>
2023-02-17 01:15:12 +0900

    [UPDATE] Catalogue alignments

--- pages/references/catalogue.tsx
+++ pages/references/catalogue.tsx
@@ -1,6 +1,5 @@
-import Description from "@/components/description";
-import InnerHeader from "@/components/innerHeader";
 import Template from "@/components/template";
+import { ArrowDownTrayIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
@@ -19,32 +18,53 @@ export default function Catalogue() {
   return (
     <>
       <Template>
-        <Link href="/catalogues/201801-V1-카다로그-인창피엠티.pdf" download>
+        <Link
+          href="/catalogues/201801-V1-카다로그-인창피엠티.pdf"
+          download
+          className="mx-12 md:mx-32 lg:mx-60 text-center border p-4 rounded-xl block"
+        >
+          <ArrowDownTrayIcon className="w-8 inline-block mr-4" />
           카달로그 다운로드 하기
         </Link>
         <br />
-        Page {pageNumber} of {numPages}
-        {pageNumber > 1 && (
-          <button onClick={() => setPageNumber((prev) => prev + -1)}>
-            이전페이지
-          </button>
-        )}
-        {pageNumber < numPages && (
-          <button onClick={() => setPageNumber((prev) => prev + +1)}>
-            다음페이지
-          </button>
-        )}
+        <br />
+        <br />
         <Document
           file="/catalogues/201801-V1-카다로그-인창피엠티.pdf"
+          className="mx-auto"
           onLoadSuccess={onDocumentLoadSuccess}
         >
-          <Page
+          {/* <Page
             //   height={600}
             width={800}
             pageNumber={pageNumber}
             renderTextLayer={false}
             renderAnnotationLayer={false}
-          />
+          /> */}
+
+          {Array.from(Array(9).keys()).map((i, idx, is) => {
+            if (i == 0) return;
+            return (
+              <div key={idx}>
+                <Page
+                  className="hidden md:inline-block"
+                  // height={600}
+                  width={800}
+                  pageNumber={i}
+                  renderTextLayer={false}
+                  renderAnnotationLayer={false}
+                />
+                <Page
+                  className="md:hidden"
+                  // height={600}
+                  width={320}
+                  pageNumber={i}
+                  renderTextLayer={false}
+                  renderAnnotationLayer={false}
+                />
+              </div>
+            );
+          })}
         </Document>
       </Template>
     </>
